b193916901dccb84c3210ef4450f2636eef91a8d
asdf99245 <asdf99245@naver.com>
2021-08-18 19:31:07 +0900

    주석 추가

--- BOJ2225-합분해/BOJ2225.js
+++ BOJ2225-합분해/BOJ2225.js
@@ -11,11 +11,13 @@ let dp, N, K;
 const mod = 1000000000;
 
 function init() {
+  // dp 배열 초기화
   dp = new Array(K + 1);
   for (let i = 0; i <= K; i++) {
     dp[i] = new Array(N + 1).fill(0);
   }
 
+  // 1개의 숫자로 나타내는 경우는 모든 수에서 1가지 밖에 없음
   for (let i = 0; i <= N; i++) {
     dp[1][i] = 1;
   }
@@ -26,6 +28,7 @@ rl.on('line', (line) => {
   rl.close();
 }).on('close', () => {
   init();
+  // 현재 n 값보다 같거나 작은값을 더한것을 뺀 숫자일때의 경우를 모두 더해줌 (k - 1번째)
   for (let i = 2; i <= K; i++) {
     for (let j = 0; j <= N; j++) {
       for (let l = 0; l <= j; l++) {
