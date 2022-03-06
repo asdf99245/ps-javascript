c68bb1c43702083a326dbf1758b294c5592985e2
asdf99245 <asdf99245@naver.com>
2022-03-06 14:02:16 +0900

    백준: 13250번 주사위게임 문제 풀이

---
+++ BOJ/BOJ13250-주사위게임/main.js
@@ -0,0 +1,26 @@
+// 주사위 게임
+// https://www.acmicpc.net/problem/13250
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let N;
+
+rl.on('line', (line) => {
+  N = parseInt(line);
+  rl.close();
+}).on('close', () => {
+  const dp = new Array(N + 1).fill(0);
+  dp[1] = 1;
+  for (let i = 2; i <= N; i++) {
+    for (let j = 1; j <= 6; j++) {
+      if (i <= j) dp[i] += 1 / 6;
+      else dp[i] += (dp[i - j] + 1) / 6; // 한번 던졌으므로 + 1
+    }
+  }
+
+  console.log(dp[N]);
+});
