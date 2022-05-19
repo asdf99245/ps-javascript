357ab3f9b5c48e4d9ba4d7e6ab2d671af5370e37
asdf99245 <asdf99245@naver.com>
2022-05-19 17:28:02 +0900

    프로그래머스: 주차 요금 계산 주석 추가

--- PROGRAMMERS/PROGRAMMERS-주차요금계산/main.js
+++ PROGRAMMERS/PROGRAMMERS-주차요금계산/main.js
@@ -6,11 +6,13 @@ function solution(fees, records) {
   const accTime = new Map();
 
   const timeToMin = (time) => {
+    // 문자열 타입의 시간을 분 단위 정수 타입으로 변경해서 반환
     const [hour, min] = time.split(':').map((i) => parseInt(i));
     return hour * 60 + min;
   };
 
   const calcFee = (min) => {
+    // 시간에 대한 요금 계산
     let fee = fees[1];
     if (min > fees[0]) {
       fee += Math.ceil((min - fees[0]) / fees[2]) * fees[3];
@@ -23,10 +25,13 @@ function solution(fees, records) {
     const [time, num, type] = record.split(' ');
     const min = timeToMin(time);
     if (type === 'IN') {
+      // 입차시 차량번호에 대한 입차 시간을 Map에 저장
       map.set(num, min);
     } else {
+      // 출차시 출차 시간과 입차 시간의 차이를 구한다.
       const spend = min - map.get(num);
       map.delete(num);
+      // 차량에 대한 주차 시간을 Map에 누적
       if (accTime.has(num)) {
         accTime.set(num, accTime.get(num) + spend);
       } else {
@@ -35,6 +40,7 @@ function solution(fees, records) {
     }
   });
 
+  // 출차 기록이 없는 차량 23:59 출차로 처리
   const lastTime = timeToMin('23:59');
   for (const [num, min] of map) {
     if (accTime.has(num)) {
@@ -47,6 +53,7 @@ function solution(fees, records) {
   for (const [num, time] of accTime) {
     answer.push([num, calcFee(time)]);
   }
+  // 차량번호 오름차순으로 정렬
   answer.sort((a, b) => parseInt(a[0]) - parseInt(b[0]));
   return answer.reduce((acc, curr) => {
     acc.push(curr[1]);
